a2b41079df7aab99e451f7580425896ce4f0eb33
Montgomory <hlmontgomory@gmail.com>
2014-12-16 14:15:43 +0800

    Clean up header.php

--- header.php
+++ header.php
@@ -1,8 +1,3 @@
-<!DOCTYPE html>
-<html>
-    <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/bootstrap.css"  type="text/css">
-    <link rel="stylesheet" href="css/style.css"  type="text/css">
-    </head>
-</html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/bootstrap.css"  type="text/css">
+<link rel="stylesheet" href="css/style.css"  type="text/css">
