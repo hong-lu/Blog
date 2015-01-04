9347acbf78a7f1a65d3b57099c8675b60d283fc1
Hong Lu <hlmontgomory@gmail.com>
2015-01-04 21:59:49 +0800

    Fixed the link of all_post on nav bar

--- src/view/common/new_nav.php
+++ src/view/common/new_nav.php
@@ -18,7 +18,7 @@ if (isset($_GET['uid'])){
 
     <div class="navbar-collapse collapse navbar-responsive-collapse">
         <ul class="nav navbar-nav">
-            <li><a href="<?php echo "all_posts.php?uid=".$myid; ?>">All My Posts</a></li>
+            <li><a href="<?php echo "all_posts.php?uid=".$_SESSION['uid']; ?>">All My Posts</a></li>
             <li><a href="new_post.php">New Post</a></li>
             <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Account Settings <b class="caret"></b></a>
