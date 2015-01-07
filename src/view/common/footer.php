a914deb1b77b8380f93f5c00801174407ca9f30c
Hong Lu <hlmontgomory@gmail.com>
2015-01-07 13:01:07 +0800

    Updated footer to 2015

--- src/view/common/footer.php
+++ src/view/common/footer.php
@@ -1,7 +1,7 @@
 <div class="row">
   <div class="col-lg-12">
     <br><br>
-      <p class="text-center"><a href="about.php">About</a><br> &copy; Copyright 2014 Hong Lu</p>
+      <p class="text-center"><a href="about.php">About</a><br> &copy; Copyright 2015 Hong Lu</p>
     <br><br>
   </div>
 </div>
